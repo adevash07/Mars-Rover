f419e77ea60dc465ce736b0f7fd9e8e46b43d2ba
debugger <58630922+adevash07@users.noreply.github.com>
2023-02-12 05:56:29 +0100

    test: 8 roverNavigation test case passed

--- src/controller/roverNavigation.test.ts
+++ src/controller/roverNavigation.test.ts
@@ -8,8 +8,50 @@ describe("Testing Rover Navigation System", () => {
       y: 0,
     },
   };
-  test("Test rover direction get steered 90deg Right, From North to South", () => {
+  test("Test rover direction get steered 90deg Right, From North to East", () => {
+    navigation.steer(apolloSearcher, "R");
+    expect(apolloSearcher.direction).toBe("E");
+  });
+
+  test("Test rover direction get steered 90deg Right, From South to North", () => {
+    apolloSearcher.direction = "S";
+    navigation.steer(apolloSearcher, "R");
+    expect(apolloSearcher.direction).toBe("W");
+  });
+
+  test("Test rover direction get steered 90deg Right, From West to North", () => {
+    apolloSearcher.direction = "W";
+    navigation.steer(apolloSearcher, "R");
+    expect(apolloSearcher.direction).toBe("N");
+  });
+
+  test("Test rover direction get steered 90deg Right, From East to South", () => {
+    apolloSearcher.direction = "E";
     navigation.steer(apolloSearcher, "R");
     expect(apolloSearcher.direction).toBe("S");
+  });
+
+  test("Test rover direction get steered 90deg Left, From North to West", () => {
+    apolloSearcher.direction = "N";
+    navigation.steer(apolloSearcher, "L");
+    expect(apolloSearcher.direction).toBe("W");
+  });
+
+  test("Test rover direction get steered 90deg Left, From South to East", () => {
+    apolloSearcher.direction = "S";
+    navigation.steer(apolloSearcher, "L");
+    expect(apolloSearcher.direction).toBe("E");
+  });
+
+  test("Test rover direction get steered 90deg Left, From West to South", () => {
+    apolloSearcher.direction = "W";
+    navigation.steer(apolloSearcher, "L");
+    expect(apolloSearcher.direction).toBe("S");
+  });
+
+  test("Test rover direction get steered 90deg Left, From East to North", () => {
+    apolloSearcher.direction = "E";
+    navigation.steer(apolloSearcher, "L");
+    expect(apolloSearcher.direction).toBe("N");
   });
 });
--- src/controller/roverNavigation.ts
+++ src/controller/roverNavigation.ts
@@ -7,7 +7,44 @@
 const navigation: Navigation<rover, steer> = {
   steer: (rover, steer) => {
     //change direction of rover steering the current direction 90deg to left if steer is L or 90deg from right if steer is R
-    rover.direction = "S";
+    if (rover.direction === "N") {
+      console.log("rover direction is N");
+      if (steer === "R") {
+        console.log(steer);
+        rover.direction = "E";
+        return void 0;
+      } else {
+        rover.direction = "W";
+        return void 0;
+      }
+    }
+    if (rover.direction === "E") {
+      if (steer === "R") {
+        rover.direction = "S";
+        return void 0;
+      } else {
+        rover.direction = "N";
+        return void 0;
+      }
+    }
+    if (rover.direction === "S") {
+      if (steer === "R") {
+        rover.direction = "W";
+        return void 0;
+      } else {
+        rover.direction = "E";
+        return void 0;
+      }
+    }
+    if (rover.direction === "W") {
+      if (steer === "R") {
+        rover.direction = "N";
+        return void 0;
+      } else {
+        rover.direction = "S";
+        return void 0;
+      }
+    }
   },
 };
 
